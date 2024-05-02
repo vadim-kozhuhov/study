e33eeff2745901c801603b10e25cae9c2456deaf
vadim.kozhuhov <vadim.kozhuhov@smartyads.com>
2024-05-02 12:39:11 +0300

    template

---
+++ homework_14/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const slider = document.getElementsByClassName('js--list');
+const prevBtn = document.getElementsByClassName('js--slider__prev');
+const nextBtn = document.getElementsByClassName('js--slider__next');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
