2183625c9a9b4845c6c3c5c103717d86b47bea6c
vadimkozuhov <vadim.kozhuhov@smartyads.com>
2024-06-06 17:39:48 +0300

    hw_22_fix

--- homework_22/home_with_webpack/src/js/index.js
+++ homework_22/home_with_webpack/src/js/index.js
@@ -1,7 +1,6 @@
 import '../scss/app.scss';
 import getData from './util';
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('222')
     getData();
     document.getElementById('reload').addEventListener('click', function (event) {
         event.target.disabled = true;
